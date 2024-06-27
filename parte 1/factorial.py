725c6f20b8f6973b3982545a5a1f40c321225ccc
Luis Angel Cruz <luis.cruz.q@uni.pe>
2024-06-27 08:04:32 -0500

    Delete parte 1 directory

--- parte 1/factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    resultado = 1
-    for i in range(1, n + 1):
-        resultado *= i
-    return resultado
-
-numero = int(input("Ingresa un número entero para calcular su factorial: "))
-print(f"El factorial de {numero} es: {factorial(numero)}")
--- parte 1/invertircadena.py
+++
@@ -1,8 +0,0 @@
-def invertir_cadena(cadena):
-    invertida = ""
-    for caracter in cadena:
-        invertida = caracter + invertida
-    return invertida
-cadena1 = "Apruebeme profe"
-cadena2 = invertir_cadena(cadena1)
-print(cadena2)
